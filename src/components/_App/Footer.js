3ac70ab1a043cc55226a0051e70d09f21fdc4a30
nightstar69 <126476107+nightstar69@users.noreply.github.com>
2024-05-16 13:43:32 +0530

    Update Footer.js

--- src/components/_App/Footer.js
+++ src/components/_App/Footer.js
@@ -24,7 +24,7 @@ const Footer = () => {
               <div className="single-footer-widget">
                 <div className="logo">
                   <Link to="/">
-                    <img src={Logo} alt="logo" width={150} />
+                    <img src={Logo} alt="logo" width={210} />
                   </Link>
                 </div>
                 <p>
