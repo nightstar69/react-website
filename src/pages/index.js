e442bdcbdaf69321d4e15d4475c61617e4861bd1
nightstar69 <126476107+nightstar69@users.noreply.github.com>
2024-08-29 07:34:05 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,83 +2,60 @@ import * as React from "react"
 import Layout from "../components/_App/layout"
 import Seo from "../components/_App/seo"
 import Navbar from "../components/_App/Navbar"
-import MainBanner from "../components/ITStartup/MainBanner"
-import Features from "../components/ITStartup/Features"
-import ServicesArea from "../components/ITStartup/ServicesArea"
-import OurFeatures from "../components/ITStartup/OurFeatures"
-// import Team from "../components/Common/Team"
-import FunFactsArea from "../components/Common/FunFactsArea"
-// import RecentWorks from "../components/Common/RecentWorks"
-// import PricingStyleOne from "../components/PricingPlans/PricingStyleOne"
-// import Feedback from "../components/Common/Feedback"
-// import Partner from "../components/Common/Partner"
-// import BlogPost from "../components/Common/BlogPost"
+
+// Lazy load components
+const MainBanner = React.lazy(() => import("../components/ITStartup/MainBanner"))
+const Features = React.lazy(() => import("../components/ITStartup/Features"))
+const ServicesArea = React.lazy(() => import("../components/ITStartup/ServicesArea"))
+const OurFeatures = React.lazy(() => import("../components/ITStartup/OurFeatures"))
+const FunFactsArea = React.lazy(() => import("../components/Common/FunFactsArea"))
+const Footer = React.lazy(() => import("../components/_App/Footer"))
+
 import aicteLogo from "../images/aicteLogo.png"
-
-// import msme from "../images/msme.png"
-
 import mca from "../images/mca.png"
-import Footer from "../components/_App/Footer"
 import { Script } from "gatsby"
 
 const IndexPage = () => (
-  
   <Layout>
-    <Script  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7614767999716149"
-     crossOrigin="anonymous"></Script>
+    <Script 
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7614767999716149"
+      crossOrigin="anonymous"
+    />
     <Navbar />
 
-    <MainBanner />
+    {/* Wrap lazy-loaded components with Suspense */}
+    <React.Suspense fallback={<div>Loading...</div>}>
+      <MainBanner />
+      <Features />
 
-    <Features />
-{/* Aicte---------- */}
-    <div className="pt-40 pb-80 mb-0 aicte" >
-        
-        <div  className="aicteDiv">
+      {/* Aicte */}
+      <div className="pt-40 pb-80 mb-0 aicte">
+        <div className="aicteDiv">
           <img
             src={aicteLogo}
-            alt="logo"
-         
+            alt="AICTE Logo"
             className="aicteImg"
-          />
-        </div><div className="aicteDiv"  >
-          <img
-            src={mca}
-            alt="logo"
-         
-            className="aicteImg"
+            loading="lazy" // Lazy load images
           />
         </div>
-    </div>
-    <ServicesArea />
+        <div className="aicteDiv">
+          <img
+            src={mca}
+            alt="MCA Logo"
+            className="aicteImg"
+            loading="lazy" // Lazy load images
+          />
+        </div>
+      </div>
 
-    <OurFeatures />
-
-    {/* <Team /> */}
-
-    {/* Youtube---------- */}
-
-    <FunFactsArea />
-
-    {/* <RecentWorks /> */}
-
-    {/* <PricingStyleOne />
-
-    <Feedback />
-
-    <Partner />
-
-    <BlogPost /> */}
- 
-    <Footer />
+      <ServicesArea />
+      <OurFeatures />
+      <FunFactsArea />
+      <Footer />
+    </React.Suspense>
   </Layout>
 )
 
-/**
- * Head export to define metadata for the page
- *
- * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
- */
 export const Head = () => <Seo title="Home" />
 
 export default IndexPage
